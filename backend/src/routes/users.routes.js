f8389ba8fe3d304ba54d40f6a0c284c00f84006c
MartinMaged30 <127694772+MaRtiNMaGed1282@users.noreply.github.com>
2026-08-14 05:08:07 +0300

    feat(authz): make user administration admin-only

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,21 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
-
 const usersController = require("../controllers/users.controller");
-
 const auth = require("../middlewares/auth.middleware");
+const authorize = require("../middlewares/authorize.middleware");
 
-const admin = require("../middlewares/admin.middleware");
+const admin = authorize("admin");
 
-router.get("/", auth, usersController.getUsers);
-
+router.get("/", auth, admin, usersController.getUsers);
 router.post("/", auth, admin, usersController.createUser);
-
 router.put("/:id/status", auth, admin, usersController.toggleUserStatus);
-
 router.put("/:id/password", auth, admin, usersController.resetPassword);
-
 router.delete("/:id", auth, admin, usersController.deleteUser);
 
 module.exports = router;
