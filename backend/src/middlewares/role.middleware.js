1497b05613aaa87a751dc099a652a71e8b0475bb
MartinMaged30 <127694772+MaRtiNMaGed1282@users.noreply.github.com>
2026-08-14 21:01:58 +0300

    Phase 23: add reusable role authorization middleware

---
+++ backend/src/middlewares/role.middleware.js
@@ -0,0 +1,17 @@
+module.exports = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({
+        message: "غير مصرح",
+      });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "ليس لديك صلاحية لتنفيذ هذا الإجراء",
+      });
+    }
+
+    next();
+  };
+};
