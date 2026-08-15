210c6eb42fe8e98e96101bb7565556c13d097c38
MartinMaged30 <127694772+MaRtiNMaGed1282@users.noreply.github.com>
2026-08-15 03:55:00 +0300

    Add authenticated client timeline route

--- backend/src/routes/activity.routes.js
+++ backend/src/routes/activity.routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const activityController = require("../controllers/activity.controller");
+const clientTimelineController = require("../controllers/client-timeline.controller");
 
 const auth = require("../middlewares/auth.middleware");
 
@@ -11,5 +12,6 @@ router.get("/", auth, activityController.getActivity);
 router.get("/case/:id", auth, activityController.getCaseActivity);
 
 router.get("/client/:id", auth, activityController.getClientActivity);
+router.get("/client/:id/timeline", auth, clientTimelineController.getClientTimeline);
 
 module.exports = router;
