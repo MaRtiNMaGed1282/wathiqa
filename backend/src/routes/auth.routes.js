f622e3114facce64e482528e9ef3f2cfd0017826
MartinMaged30 <127694772+MaRtiNMaGed1282@users.noreply.github.com>
2026-08-15 04:43:28 +0300

    Add secure logout endpoints

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const router = express.Router();
-
 const auth = require("../middlewares/auth.middleware");
-const { login, changePassword } = require("../controllers/auth.controller");
+const { login, logout, changePassword } = require("../controllers/auth.controller");
 
 router.post("/login", login);
-
+router.post("/logout", auth, logout);
+router.post("/logout-all", auth, require("../controllers/auth.controller").logoutAll);
 router.post("/change-password", auth, changePassword);
 
 module.exports = router;
